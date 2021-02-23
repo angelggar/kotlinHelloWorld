b3b81c5fcace7c7d6b39fd0bb681845231640a96
Angel Garcia Garcia <angelgg91@hotmail.com>
2021-02-23 19:48:42 +0100

    First commit

---
+++ src/HelloWorldKt.kt
@@ -0,0 +1,10 @@
+class HelloWorldKt {
+    internal object HelloWorldKt {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            var language = "French"
+            val score = 95
+            println("Hello, World!" + language)
+        }
+    }
+}
