f1f233f70e60395f453bf02ae5b74e673acad378
Angel Garcia Garcia <angelgg91@hotmail.com>
2021-03-01 13:00:01 +0100

    Variables

--- src/HelloWorldKt.kt
+++ src/HelloWorldKt.kt
@@ -2,9 +2,19 @@ class HelloWorldKt {
     internal object HelloWorldKt {
         @JvmStatic
         fun main(args: Array<String>) {
-            var language = "French"
+            var language = "English"
+            var language2: String = "Spanish"
             val score = 95
             println("Hello, World!" + language)
+
+
+            //var one //Error
+            var one : Int
+
+            language2 = "2"
+            language = "Bye"
+            println("Hello, World!" + language)
+
         }
     }
 }
